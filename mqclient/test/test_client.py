7fc55ca0d30466c316c2fdd36ee4757aec4bfb05
IMTAI <innerp@outlook.com>
2013-04-11 19:30:13 +0800

    odify call_back function name

--- mqclient/test/test_client.py
+++ mqclient/test/test_client.py
@@ -5,12 +5,12 @@ Created on 2013-4-11
 '''
 from mqclient.client import MQClient
 
-def call_bakc(body,header):
+def call_back(body,header):
     print body
     for key in header:
         print 'key:%s,value:%s'%(key,header[key])
 if __name__ == '__main__':
     client = MQClient('localhost',61613,'system','manager')
-    client.subscribe('/topic/foo', 'mqclient', call_bakc)
+    client.subscribe('/topic/foo', 'mqclient', call_back)
     client.start()
     pass
