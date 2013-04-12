618ad302255fb4fd0be76fe46f81c37bfe2c73b1
WongTai <innerp@outlook.com>
2013-04-12 17:29:28 +0800

    update start.sh

--- mqclient/test/start.sh
+++ mqclient/test/start.sh
@@ -1,3 +1,20 @@
-#!/bin/sh
-#通过nohup命令支持即使关闭当前窗口进程也依然会运行
-nohup ./test_client.py >print.out &
+#!/bin/bash
+#nohup ./test_client.py>print.out & 
+echo "[INFO] close binder...."
+pid_list=`ps -ef |grep test_client |grep -v grep | awk '{print $2}'`
+if [ -n "$pid_list" ];then
+	echo "[INFO] the existing binder ara $pid_list"
+	for pid in $pid_list;do
+		kill -9 $pid
+		sleep 2
+		echo "[INFO] closed binder with pid $pid"
+	done
+	echo "[INFO] close binder successfully"
+else
+	echo "[INFO] no binder is existing"
+fi
+echo "[INFO] start binder"
+nohup ./test_client.py>print.out &
+echo `ps -ef | grep test_client | grep -v grep`
+echo "[INFO] binder is starting ...."
+
